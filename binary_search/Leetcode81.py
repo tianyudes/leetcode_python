be1cc8047bf025869f809757a9484fa16ce5676b
tianyudes <tian18351898629@gmail.com>
2023-11-29 10:09:01 +0900

    /usr/bin/python3

--- binary_search/Leetcode81.py
+++ binary_search/Leetcode81.py
@@ -6,7 +6,26 @@ class Solution(object):
         :rtype: bool
         """
         l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (l-r) // 2
+            if nums[mid] == target: return True
 
+            while l < mid and nums[l] == nums[mid]:
+                l += 1
+            while r > mid and nums[r] == nums[mid]:
+                r -= 1
+
+            if nums[l] <= nums[mid]:
+                if nums[l] <=target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else: 
+                    r = mid - 1 
+        return False
 
 solution = Solution()
-print(solution.searchRange([5,7,7,8,8,10], 0))
+print(solution.search([1,0,1,1,1], 0))
